b7ffbc6662fd5773d50b02a147d66d9943ab3bb6
particus17 <packDparker@gmail.com>
2022-10-27 19:38:30 -0700

    fixed swiper in indentify section, mobile hamburger menu WIP

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+//mobile hamburger menu
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+  //make the button do stuff
+openMenu.addEventListener('click', show)
+closeMenu.addEventListener('click', close)
+
+function show(){
+  mainMenu.style.display = 'flex';
+  mainMenu.style.top = '0';
+}
+function close(){
+  mainMenu.style.top = '-100%';
+}
+
+
+
+//identify slider
 var swiper = new Swiper(".slide-content", {
     slidesPerView: 1,
     spaceBetween: 30,
@@ -11,3 +31,4 @@ var swiper = new Swiper(".slide-content", {
       prevEl: ".swiper-button-prev",
     },
   });
+
